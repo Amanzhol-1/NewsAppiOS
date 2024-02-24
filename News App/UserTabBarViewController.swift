ee6356c28eebc5b7ef4676aa625c4ea30f699d89
syrlybajramazan <syrlybai056@gmail.com>
2024-02-25 01:14:33 +0600

    upd

--- News App/UserTabBarViewController.swift
+++ News App/UserTabBarViewController.swift
@@ -18,26 +18,25 @@ class UserTabBarViewController: UITabBarController {
     private func configureTabs(){
         let vc1 = ViewController()
         let vc2 = FavouriteViewController()
-        let vc3 = HeadlinesViewController()
+
         let vc4 = ProfileViewController()
         vc1.tabBarItem.image = UIImage(systemName: "house.circle")
         vc2.tabBarItem.image = UIImage(systemName: "star")
         vc1.title = "Home"
         vc2.tabBarItem.title = "Favourite"
         
-        vc3.title = "headlines"
-        vc3.tabBarItem.image = UIImage(systemName: "headlight.high.beam.fill")
+       
         vc4.title = "profile"
         vc4.tabBarItem.image = UIImage(systemName: "person.circle.fill")
         let nav1 = UINavigationController(rootViewController: vc1)
         let nav2 = UINavigationController(rootViewController: vc2)
-        let nav3 = UINavigationController(rootViewController: vc3)
+        
         let nav4 = UINavigationController(rootViewController: vc4)
         
         tabBar.tintColor = .label
         tabBar.backgroundColor = .systemGray
         
-        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
+        setViewControllers([nav1, nav2, nav4], animated: true)
     }
     
 
