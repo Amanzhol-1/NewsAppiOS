bafec0e090e3ac3c1857d7ae6647686432f85b8f
AigerimKubeyeva <123069269+AigerimKubeyeva@users.noreply.github.com>
2024-02-26 02:41:42 +0600

    Add files via upload

--- News App/NewsTableViewCell.swift
+++ News App/NewsTableViewCell.swift
@@ -24,17 +24,18 @@ class NewsTableViewCellViewModel {
 
 class NewsTableViewCell: UITableViewCell {
     static let identifier = "NewsTableViewCell"
-    private var viewModel: NewsTableViewCellViewModel?
-
-    private lazy var newsTitleLabel: UILabel = {
+    private var article: Article?
+    
+    private let newsTitleLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
-        label.font = UIFont(name: "URWGeometric-Medium", size: 15)
-        label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        label.font = UIFont(name: "Proxima Nova", size: 17)
+    
+        
         return label
     }()
 
-    private lazy var newsImageView: UIImageView = {
+    private let newsImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.layer.cornerRadius = 6
         imageView.layer.masksToBounds = true
@@ -44,11 +45,9 @@ class NewsTableViewCell: UITableViewCell {
         return imageView
     }()
     
-    private lazy var newsSavedView: UIImageView = {
+    private let newsSavedView: UIImageView = {
         let imageView = UIImageView(image: UIImage(systemName: "bookmark.fill"))
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(NewsTableViewCell.imageTapped))
-        imageView.isUserInteractionEnabled = true
-        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
         return imageView
     }()
 
@@ -66,7 +65,10 @@ class NewsTableViewCell: UITableViewCell {
         contentView.layer.borderWidth = 1
         contentView.layer.borderColor = UIColor(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1.0).cgColor
         
-    
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(NewsTableViewCell.imageTapped))
+        newsSavedView.isUserInteractionEnabled = true
+        newsSavedView.addGestureRecognizer(tapGestureRecognizer)
+
     }
 
     required init?(coder: NSCoder) {
@@ -75,9 +77,6 @@ class NewsTableViewCell: UITableViewCell {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        newsTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        newsImageView.translatesAutoresizingMaskIntoConstraints = false
-        newsSavedView.translatesAutoresizingMaskIntoConstraints = false
 
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20))
         
@@ -102,19 +101,26 @@ class NewsTableViewCell: UITableViewCell {
         super.prepareForReuse()
         newsTitleLabel.text = nil
         newsImageView.image = nil
-        viewModel = nil
     }
     
     @objc func imageTapped(){
-        guard let viewModel = viewModel else { return }
-        let isSaved = DBController.shared.saveOrDeleteArticle(article: viewModel.article)
-        newsSavedView.tintColor = isSaved ? .orange : .blue
+        if let data = article{
+            let isSaved = DBController.shared.saveOrDeleteArticle(article: data)
+            if isSaved{
+                newsSavedView.tintColor = .orange
+            }else{
+                newsSavedView.tintColor = .blue
+            }
+        }
     }
 
     func configure(with viewModel: NewsTableViewCellViewModel) {
-        self.viewModel = viewModel
         newsTitleLabel.text = viewModel.article.title
-        newsSavedView.tintColor = viewModel.isSaved ? .orange : .blue
+        article = viewModel.article
+        
+        if viewModel.isSaved{
+            newsSavedView.tintColor = .orange
+        }
 
         // Image
         if let data = viewModel.imageData {
